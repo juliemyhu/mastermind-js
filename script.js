ba5475b1f99fa1e3a82eab19d38c5b376ff4e95b
Julie Hu <jhu21495@gmail.com>
2021-04-08 15:06:26 -0700

    working code and plays again

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const answer = document.querySelector('.answer')
 // form class to add guess
 const addGuesses = document.querySelector('.add-guesses');
 //guess container
-const guessesList = document.querySelector('.guesses-container');
+const guessesContainer = document.querySelector('.guesses-container');
 // store guesses in session
 const guesses = JSON.parse(localStorage.getItem('guesses')) || [];
 // store code in session 
 var code = JSON.parse(localStorage.getItem('code')) || [];
 
+getCode()
 
 function getCode() {
     fetch("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new", {})
@@ -15,9 +16,11 @@ function getCode() {
     .then(function store (html) {
         html = html.replace(/\n/g, '');
         localStorage.setItem('code', JSON.stringify(html));
-        code = JSON.parse(localStorage.getItem('code'));
+        console.log('Hello')
+        code = JSON.parse (localStorage.getItem('code'));
         }  
-    ) 
+    ) ;
+    return code
 }
 
 console.log("code",code)
@@ -26,9 +29,9 @@ console.log("code",code)
 // 0 correct color, wrong location
 // 1 correct color, correct location
 function checkStatus(guess,code) {
+    console.log(code,guess)
     var result = []
-    console.log("code:",code)
-    console.log("guess",guess)
+
     for (var i = 0 ; i< code.length;i++){
         if (guess[i] === code[i]) {
             result.push(1)
@@ -36,17 +39,23 @@ function checkStatus(guess,code) {
             result.push(0)
         } 
     }
-    console.log(typeof(result));
     return result
 }
 
 function gameOver(guesses, status) {
-    console.log(status);
-    console.log(guesses);
-    if (JSON.stringify(status) == JSON.stringify([1,1,1,1])) {
-        alert("You won")
-    } else if (guesses.length === 10 ) {
+
+    status = JSON.stringify(status)
+    verifyCode = JSON.stringify([1,1,1,1])
+
+    if (status == verifyCode) {
+        var confirmPlay = confirm("Congrats, You won! Click ok to play again")
+        if (confirmPlay) {
+            playAgain();
+            // location.reload();
+          }
+    } else if (guesses.length >= 10 ) {
         alert("game over, you lose")
+        playAgain();
     }
 }
 function addItem(e) {
@@ -60,7 +69,7 @@ function addItem(e) {
     };
     this.reset();
     guesses.push(item);
-    populateList(guesses, guessesList);
+    populateList(guesses, guessesContainer);
 
     localStorage.setItem('guesses', JSON.stringify(guesses));
 
@@ -68,25 +77,27 @@ function addItem(e) {
     
   }
 
-function populateList(guesses = [], guessesList) {
-guessesList.innerHTML = guesses.map((guess, i) => {
-    return `
-    <li>
-        <label for="item${i}">${guess.text}</label>
-        <label for="item${i}">${guess.status}</label>
-    </li>
-    `;
-}).join('');
+function populateList(guesses = [], guessesContainer) {
+    guessesContainer.innerHTML = guesses.map((guess, i) => {
+        return `
+        <li>
+            <label for="item${i}">${guess.text}</label>
+            <label for="item${i}">${guess.status}</label>
+        </li>
+        `;
+    }).join('');
 }
 
 
 function playAgain () {
     localStorage.clear();
-    getCode();
+    code = getCode();
+    location.reload();
+    console.log(code)
 }
 
 addGuesses.addEventListener('submit', addItem);
 
-populateList(guesses, guessesList);
+populateList(guesses, guessesContainer);
 console.log(localStorage.getItem('guesses'))
-//*[@id="invisible"]/pre
+
